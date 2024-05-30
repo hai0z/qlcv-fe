952479a52e18882ca4f88aa5b6df3cd4e04f26d5
hai0z <hainguyen56211@gmail.com>
2024-05-30 21:04:53 +0700

    update favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Favicon from "react-favicon";
 import { NextUIProvider } from "@nextui-org/system";
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
@@ -7,11 +8,12 @@ import router from "./routes";
 import "./index.css";
 
 const root = createRoot(document.getElementById("app"));
-
+const iconUrl = "https://cdn-icons-png.flaticon.com/128/5253/5253896.png";
 root.render(
   <NextUIProvider>
-    <ThemeProvider enableSystem defaultTheme="plus">
+    <ThemeProvider>
       <RouterProvider router={router} />
+      <Favicon url={iconUrl} iconSize={64} />
     </ThemeProvider>
   </NextUIProvider>
 );
